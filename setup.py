77718c84e2998663980e68873ee2338d25b89cea
raphaelrpl <raphael.wcosta@gmail.com>
2019-07-05 15:26:15 -0300

    Up initial template for bdc_theme

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+setup(
+    name='bdc_theme',
+    version='0.1',
+    description='Brazil Data Cube Theme for Sphinx readthedocs.',
+    long_description=open('README.md').read(),
+    author='Admin',
+    author_email='admin@admin.com',
+    url='https://github.com/brazil-data-cube/bdc-readthedocs-theme',
+    packages=['bdc_theme'],
+    include_package_data=True,
+    install_requires=['Sphinx>2'],
+    license="MIT"
+)
