945a609b69114f83e64a727d6225882933ade1f6
raphaelrpl <raphael.wcosta@gmail.com>
2019-10-10 13:44:08 -0300

    Code review: rename

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 
 setup(
-    name='bdc_theme',
-    version='0.1',
+    name='bdc-readthedocs-theme',
+    version='0.1.1',
     description='Brazil Data Cube Theme for Sphinx readthedocs.',
     long_description=open('README.md').read(),
     author='Admin',
     author_email='admin@admin.com',
     url='https://github.com/brazil-data-cube/bdc-readthedocs-theme',
-    packages=['bdc_theme'],
+    packages=find_packages(),
     include_package_data=True,
     install_requires=['Sphinx>2'],
     license="MIT"
